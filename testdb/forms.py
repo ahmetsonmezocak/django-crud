f2c91fce6fef4c665c12f66a031a1cd24ec07eb1
Ahmet SÖNMEZOCAK <ahmet_sonmezocak@hotmail.com>
2023-03-19 14:54:33 +0300

    CRUD operations completed for all tables

--- testdb/forms.py
+++ testdb/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Lesson
+from .models import Student
+from .models import Teacher
 
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
+        fields = "__all__"
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
         fields = "__all__"
--- testdb/urls.py
+++ testdb/urls.py
@@ -4,9 +4,18 @@ from . import views
 urlpatterns = [
     path("", views.read),
     path("read", views.read),
+    path("index", views.read),
     # path("<id>/delete", views.delete),
     path("delete/<int:id>", views.delete),
+    path("deleteStudent/<int:id>", views.deleteStudent),
+    path("deleteTeacher/<int:id>", views.deleteTeacher),
     path("create", views.create),
+    path("createStudent", views.createStudent),
+    path("createTeacher", views.createTeacher),
     path("update/<int:id>", views.update),
     path("update/updaterecord/<int:id>", views.updaterecord),
+    path("updateStudent/<int:id>", views.updateStudent),
+    path("updateStudent/updaterecordstudent/<int:id>", views.updaterecordstudent),
+    path("updateTeacher/<int:id>", views.updateTeacher),
+    path("updateTeacher/updaterecordteacher/<int:id>", views.updaterecordteacher),
 ]
