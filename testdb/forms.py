2f03965f462a9434648e09f4c1612fb796a29c14
Ahmet SÖNMEZOCAK <ahmet_sonmezocak@hotmail.com>
2023-04-03 16:22:09 +0300

    'static' files(css, js) created, TeacherForm in forms.py edited

--- testdb/forms.py
+++ testdb/forms.py
@@ -8,12 +8,27 @@ class LessonForm(forms.ModelForm):
         model = Lesson
         fields = "__all__"
 
+class CustomMMCF(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, lessons):
+        return "%s" % lessons.name
+    
+class CustomMMCF2(forms.ModelChoiceField):
+    def label_from_instance(self, lesson):
+        return "%s" % lesson.name
+
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = "__all__"
+    lessons = CustomMMCF(
+        queryset=Lesson.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
 
 class TeacherForm(forms.ModelForm):
     class Meta:
         model = Teacher
         fields = "__all__"
+    lesson = CustomMMCF2(
+        queryset=Lesson.objects.all()
+    )
